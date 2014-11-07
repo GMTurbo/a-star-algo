ec7cd9e4e18f2f5c51a1f72a8ac1345dd1b0aad5
Gabe Testa <gmturbo@me.com>
2014-11-07 08:43:24 -0700

    node update

--- js/node.js
+++ js/node.js
@@ -1,5 +1,28 @@
 
-//do this one using prototype stuff
-function Node(options){
+var Node = function(options){
+  options = _.defaults(options, {
+    x: 0.1,
+    y: 100,
+    h: 0,
+    g: 0
+  });
+  // hueristic (h), movement cost (g), f-val (g+h), parent (a node to reach this node)
+  this.H = options.h;
+  this.G = options.g;
+  this.F = this.H + this.G;
+  this.parent = null;
+};
 
-}
+Node.prototype.setMovementCost = function(g){
+  this.G = g;
+  this.F = this.H + this.G;
+};
+
+Node.prototype.setHueristic = function(h){
+  this.H = h;
+  this.F = this.H + this.G;
+};
+
+Node.prototype.setParent = function(parent){
+  this.parent = parent;
+};
