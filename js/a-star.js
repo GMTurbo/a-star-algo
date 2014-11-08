458f8fbae8347a552489ab9fe8deabbe1815057e
GMTurbo <gmturbo@me.com>
2014-11-08 13:24:37 -0700

    updates

--- js/a-star.js
+++ js/a-star.js
@@ -25,22 +25,61 @@ var System = function(options) {
     context = canvas.getContext('2d'),
     initialSetup = true,
     isMobile = options.isMobile,
+    start, end,
     mesh = [];
 
   var setup = function() {
+    //heuristic value of each node can
+    // be calculate once because we are using
+    // a static mesh
+    // heuristic of each node is dependent
+    // on start and stop location of path
+
+    //so let's randomly select the start and
+    // stop locations
 
     mesh = [];
 
     $(canvas).attr('width', width).attr('height', height);
 
-    var count = width * height;
+    var count = width * height,
+      scale = 20;
 
-    for (var i = 0; i < width; i++) {
-      for (var j = 0; j < height; j++) {
+    start = getStartLocation({
+        xRange: {
+          min: 0,
+          max: ~~(width / scale)
+        },
+        yRange: {
+          min: 0,
+          max: ~~(height / scale)
+        }
+      }),
+      end = getEndLocation({
+        xRange: {
+          min: 0,
+          max: ~~(width / scale)
+        },
+        yRange: {
+          min: 0,
+          max: ~~(height / scale)
+        }
+      });
+
+    for (var i = 0, x = ~~(width / scale); i <= x; i++) {
+      for (var j = 0, y = ~~(height / scale); j <= y; j++) {
         mesh.push(new Node({
-          x: i,
-          y: j,
-
+          x: i * scale,
+          y: j * scale,
+          width: ~~(scale/2),
+          h: calculateHeuristic({
+            x: i * scale,
+            y: j * scale
+          }, end),
+          g: calculateMoveCost(start, {
+            x: i * scale,
+            y: j * scale
+          })
         }));
       }
     }
@@ -51,7 +90,57 @@ var System = function(options) {
     }
   };
 
-  return {
+  function calculateHeuristic(current, target) {
+    //manhattan heuristic is just the number of
+    // nodes between the node and the target
+    return Math.abs(current.x - target.x) + Math.abs(current.y - target.y)
+  };
 
+  function calculateHeuristic(current, target) {
+    // 10 for nodes next too
+    // 14 for nodes diagonal too
+    return (Math.abs(current.x - target.x) == 1 **)
+  };
+
+  function getStartLocation(area) {
+    if (!start) {
+      start = {
+        x: area.xRange.min,
+        y: area.yRange.min
+      }
+    }
+    return start;
+  };
+
+  function getEndLocation(area) {
+    if (!end) {
+      end = {
+        x: area.xRange.max,
+        y: area.yRange.max
+      }
+    }
+    return end;
+  };
+
+  function updateNodes(){
+    // get current
+    // open 
+  };
+
+  function drawSystem() {
+    context.clearRect(0, 0, width, height);
+    _.forEach(mesh, function(node) {
+      node.draw(context);
+    });
+  };
+
+  function updateSystem() {
+    updateNodes();
+    drawSystem();
+    reqFrame(updateSystem);
+  };
+
+  return {
+    begin: setup
   }
 };
--- js/node.js
+++ js/node.js
@@ -1,30 +1,87 @@
-
-var Node = function(options){
+var Node = function(options) {
   options = _.defaults(options, {
     x: 0.1,
     y: 100,
     h: 0,
-    g: 0
+    g: 0,
+    width: 1
   });
   // hueristic (h), movement cost (g), f-val (g+h), parent (a node to reach this node)
   this.x = options.x,
-  this.y = options.y,
-  this.H = options.h,
-  this.G = options.g,
-  this.F = this.H + this.G,
-  this.parent = null;
+    this.y = options.y,
+    this.H = options.h,
+    this.G = options.g,
+    this.F = this.H + this.G,
+    this.parent = null,
+    this.open = 1,
+    this.width = options.width,
+    this.opacity = 128;
 };
 
-Node.prototype.setMovementCost = function(g){
+Node.prototype.getColor = function() {
+  // switch (TYPE) {
+  //   case 'captured':
+  //     return 'rgba(255,0,0,' + opacity + ')';
+  //   case 'cop':
+  //     return 'rgba(171,241,55,' + opacity + ')';
+  //   case 'robber':
+  //     return 'rgba(254,255,189,' + opacity + ')';
+  // }
+
+  return 'rgba(171,241,55,' + this.opacity + ')';
+};
+
+Node.prototype.getShadowColor = function() {
+  // switch (TYPE) {
+  //   case 'captured':
+  //     return 'rgba(0,0,0,1)';
+  //   case 'cop':
+  //     return 'rgba(0,0,0,1)';
+  //   case 'robber':
+  //     return 'rgba(0,0,0,1)';
+  // }
+  return 'rgba(0,0,0,1)';
+};
+
+Node.prototype.setMovementCost = function(g) {
   this.G = g;
   this.F = this.H + this.G;
 };
 
-Node.prototype.setHueristic = function(h){
+Node.prototype.setHueristic = function(h) {
   this.H = h;
   this.F = this.H + this.G;
 };
 
-Node.prototype.setParent = function(parent){
+Node.prototype.setParent = function(parent) {
   this.parent = parent;
+};
+
+Node.prototype.draw = function(context) {
+
+  var pnt = [this.x, this.y];
+
+  context.beginPath();
+
+  context.fillStyle = this.getColor();
+  context.shadowColor = this.getShadowColor();
+  context.arc(pnt[0] - this.width, pnt[1] - this.width, this.width, 0, 2 * Math.PI, false);
+
+  context.closePath();
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 0;
+  context.shadowBlur = 10;
+  context.fill();
+
+  // context.lineWidth = 1;
+  // context.strokeStyle = this.getColor();
+  // for (var i = 0, length = path.length; i < length - 1; i++) {
+  //   context.moveTo(path[i][0] - r, path[i][1] - r);
+  //   context.lineTo(path[i + 1][0] - r, path[i + 1][1] - r);
+  // }
+
+  context.stroke();
+
+  context.closePath();
+
 };
